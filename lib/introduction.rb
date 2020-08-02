386163c18cf6e8debb989d7f1c8d36a6e1ce00ae
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-02 15:04:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,9 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
